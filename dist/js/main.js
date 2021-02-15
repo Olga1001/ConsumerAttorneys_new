43c3daa8e5cc4dca4eb1bdd9e7287f531823a326
Olga <zhuravel.olga95@gmail.com>
2021-02-15 19:16:33 +0200

    added new page

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,4 +20,14 @@ $(document).ready(function () {
     $(this).toggleClass('active').parents().siblings().find(".asked-action").removeClass('active');
     $(this).siblings().slideToggle(300).parents().siblings().find(".asked-dropdown").slideUp(300);
   });
+
+  window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 1000); // первичный scroll
+
+    window.scrollBy(0, 1);
+  };
 });
